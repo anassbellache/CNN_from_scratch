357c95ff07a368ecccbce17c5a0380217054db61
Anass Bellechhab <bel_ans@hotmail.com>
2020-03-01 20:45:28 +0100

    conv operation

---
+++ layers/conv.py
@@ -0,0 +1,38 @@
+import numpy as np
+from utils import pad_1d, pad_2d
+
+class conv2D:
+    def __init__(self, num_filters, filter_size, padding=0):
+        self.num_filters = num_filters
+        self.filter_size = filter_size
+        self.padding = padding
+        self.conv_filter = np.random.rand(num_filters, filter_size, filter_size)/(filter_size*filter_size)
+    
+    def image_patch(self, image):
+        height, width, _ = image.shape
+        self.current_image = image
+        for j in range(height - self.filter_size + 1):
+            for k in range(width - self.filter_size + 1):
+                image_patch = image[j : (j + self.filter_size), k:(k+self.filter_size)]
+                yield image_patch, j, k
+    
+    def forward(self, image):
+        padded_image = pad_2d(image, self.padding)
+        height, width = padded_image.shape
+        output = np.zeros((height - self.filter_size + 1, width - self.filter_size + 1, self.num_filters))
+        for patch, i, j in self.image_patch(image):
+            output[i,j] = np.sum(patch*self.conv_filter, axis=(1,2))
+        return output
+
+    def backward(self, previous_grad, learning_rate):
+        conv_grad = np.zeros(self.conv_filter.shape)
+        for patch, i, j in self.image_patch(self.current_image):
+            for k in range(self.num_filters):
+                conv_grad[k] += patch*previous_grad[i,j,k]
+        
+        self.conv_filter -= learning_rate*conv_grad
+        return conv_grad
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def pad_1d(input_, padding):
+    zeros = np.array([0])
+    zeros = np.repeat(zeros, padding)
+    return np.concatenate([zeros, input_, zeros])
+
+def pad_2d(input_, padding):
+    side_pad = np.zeros(input_.shape[0], padding)
+    side_padded = np.concatenate([side_pad, input_, side_pad])
+    ceiling_pad = np.zeros(padding, input_.shape[1] + 2*padding)
+    full_padded = np.concatenate([ceiling_pad, side_padded, ceiling_pad],axis=1)
+    return full_padded
+
+def batch_pad_2d(batch, padding):
+    outs = [pad_2d(elt, padding) for elt in batch]
+    return np.stack(outs)
